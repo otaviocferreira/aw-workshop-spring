03dc35cef40d86a4cea0e566fc701b9965459c12
otaviocferreira <otavio.cferreira@gmail.com>
2017-02-16 11:45:07 -0200

    Add files via upload

---
+++ src/main/java/com/algaworks/vinhos/repository/Vinhos.java
@@ -0,0 +1,8 @@
+package com.algaworks.vinhos.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.vinhos.model.Vinho;
+
+public interface Vinhos	extends JpaRepository<Vinho, Long> {
+
+}
